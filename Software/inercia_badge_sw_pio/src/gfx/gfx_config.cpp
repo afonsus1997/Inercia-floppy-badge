ec4823c2c4da390d0a44a149b56f59202fbe0817
Luís Correia <luis.f.correia@gmail.com>
2025-02-08 22:57:28 +0000

    Add new animation as default

--- Software/inercia_badge_sw_pio/src/gfx/gfx_config.cpp
+++ Software/inercia_badge_sw_pio/src/gfx/gfx_config.cpp
@@ -2,7 +2,8 @@
 #include "gfx_config.h"
 
 
-const int kNumAnimations = 1; // Total animations
+const int kNumAnimations = 2; // Total animations
 
 AnimationFunction animations[] = { 
+  AnimHyperspace,
   AnimRotatingLogo};
--- Software/inercia_badge_sw_pio/src/gfx/gfx_config.h
+++ Software/inercia_badge_sw_pio/src/gfx/gfx_config.h
@@ -2,6 +2,7 @@
 #define __GFX_CONFIG__H__
 
 #include "animations/rotating_logo/anim_rotating_logo.h"
+#include "animations/hyperspace/anim_hyperspace.h"
 
 typedef void (*AnimationFunction)(); // Define a type for the function pointers
 
